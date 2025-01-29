96a63b6664c27b3ad31b64cf1998a7f94dfc51b9
Raman-Shakya <raman.gwarko15@gmail.com>
2025-01-29 19:45:19 +0545

    Added: align-self property in div

--- src/app/component/previewDivComponent.jsx
+++ src/app/component/previewDivComponent.jsx
@@ -1,24 +1,38 @@
 'use client';
 
 import { useState } from "react";
+import SettingComponent from "./settingComponent";
 
 const PreviewDivComponent = ({ index }) => {
     const [flex, setFlex] = useState('none');
+    const [alignSelf, setAlignSelf] = useState('flex-start');
 
     return (
         <div 
-            style={{ flex: flex }} 
-            className="flex flex-col items-center justify-center w-[250px] h-[250px] py-10 px-5 bg-[--card-background] rounded-[50px] shadow transition-all"
+            style={{ flex: flex, alignSelf: alignSelf }} 
+            className="flex flex-col items-center justify-center w-[250px] min-h-[250px] py-10 px-5 bg-[--card-background] rounded-[50px] shadow transition-all"
         >
             <h1 className="text-5xl pb-4 font-bold">Div<span>{index}</span></h1>
             <div>flex:</div>
             <div>
                 <input 
-                    className="w-[60px] text-center py-1 px-2 rounded-xl outline-none"
+                    className="w-[200px] text-center py-1 px-2 rounded-xl outline-none"
                     value={flex} 
                     onInput={(e) => setFlex(e.target.value)}
                 />
             </div>
+            <div>align-self:</div>
+            <select
+                onInput={(e) => setAlignSelf(e.target.value)}
+                className="border border-[--card-background] w-[200px] text-center py-1 px-2 rounded-xl outline-none"
+                value={alignSelf}
+            >
+                <option>stretch</option>
+                <option>flex-start</option>
+                <option>flex-end</option>
+                <option>center</option>
+                <option>baseline</option>
+            </select>
         </div>
     )
 }
